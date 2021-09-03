4db243dcf592296a47fb973753a20f557e31a715
emma-elise <loirratse@gmail.com>
2021-09-03 06:07:53 -0400

    app initialized; beginning route handling

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan("dev"));
 
-const PORT = 1337;
+// app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html = `<!DOCTYPE HTML>
+    <html>
+    <head>
+      <title>Wizard News</title>
+    </head>
+    <body>
+      <p>Above post test</p>
+      <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        )
+        .join("")}
+    </div>  
+      <p>Below post test</p>
+    </body>
+    </html>`;
+  res.send(html);
+});
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
+// ${posts.map(post => { `<li>${/* */}</li>`; )}
+
+// { post.list: list, post.find: find }
+console.log("hi");
+
+// <ul>
+//   $
+//   {posts.map((post) => {
+//     `<li>${post.data}</li>`;
+//   })}
+// </ul>;
